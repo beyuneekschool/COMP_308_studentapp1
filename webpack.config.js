9fae9d7eab8ff68f6004ffa080103701164a06a8
Parth Sharma <parthsharma11847@gmail.com>
2024-04-02 17:18:22 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const Dotenv = require('dotenv-webpack');
+const deps = require("./package.json").dependencies;
+const path = require('path');
+
+module.exports = (_, argv) => {
+
+  const isProduction = argv.mode === 'production';
+
+  const publicPath = isProduction
+    ? 'https://comp308-studentapp1.netlify.app/'
+    : 'http://localhost:3000/';
+
+  return {
+    output: {
+      publicPath: publicPath,
+      path: path.resolve(__dirname, 'dist'), 
+      clean: true, 
+    },
+
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
+
+    devServer: {
+      port: 3000,
+      historyApiFallback: true,
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
+        },
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "studentapp1",
+        filename: "remoteEntry.js",
+        remotes: {},
+        exposes: {
+          './StudentForm': './src/StudentForm',
+        },
+        shared: {
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: deps["react-dom"],
+          },
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
+      new Dotenv()
+    ],
+  };
+};
